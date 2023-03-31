19b7b7ddcbcd447af7ad94a63e75bdb6466adaca
diego <diegonegocios21@outlook.com>
2023-03-31 19:26:09 -0300

    primeirocommit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-let changeTitleGreen = document.getElementById("title_green");
-
-changeTitleGreen.onmouseover = function () {
-    changeTitleGreen.innerHTML = "Faça sua parte, Save the World"
+function titleGreen() {
+    let changeTitleGreen = document.getElementById("title_green");
+    let returnTitleGreen = document.getElementById("title_green")
+    changeTitleGreen.onmouseover = function () {
+        changeTitleGreen.innerHTML = "Faça sua parte, Save the World"
+    }
+    returnTitleGreen.onmouseout = () => {
+        changeTitleGreen.innerHTML = "Bem vindo a GreenSave"
+    }
 }
+
+titleGreen()
+
+function buttonGreen() {
+    let changeColorButton = document.getElementById("buttonform");
+    let returnColorButton = document.getElementById("buttonform")
+    changeTitleGreen.onmouseover = () => {
+        changeColorButton.style.backgroundColor = 'rgb(55, 136, 57)'
+    }
+    returnColorButton.onmouseout = () => {
+        returnColorButton.style.backgroundColor = ''
+    }
+}
+
+buttonGreen()
